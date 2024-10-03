85a6f63af723af058b8c64f0ad1ede3b13357391
Tamil-C0de <tamil.greyhat03@gmail.com>
2024-10-03 14:53:12 +0530

    Add files via upload

--- Django/modelProject/modelApp/views.py
+++ Django/modelProject/modelApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        author = request.POST['author']
+        publication_date = request.POST['publication_date']
+        price = request.POST['price']
+        stock = request.POST['stock']
+        
+        new_book = Book(
+            title=title,
+            author=author,
+            publication_date=publication_date,
+            price=price,
+            stock=stock
+        )
+        new_book.save()
+    return render(request, 'modelApp/add_book.html')
